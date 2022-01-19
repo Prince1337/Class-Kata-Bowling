b0a23c07145e332c60ecc5db028d1eb859334cff
Prince1337 <prince.pieritz@gmail.com>
2022-01-19 23:12:44 +0100

    tests:refactoring

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,11 +1,3 @@
 
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-S
-#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
-K
-src/test/java/GameTest.java,4\e\4ebbdfdc8eb06f4ed2b0b122b53cc9c1bbfadf05
-H
-src/main/java/Frame.java,7\6\76095fd65a421e6a435f1d14ac99be9a946b7c83
-G
-src/main/java/Game.java,5\a\5a469ce477081efcec98a963c18526f991f62425
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -9,500 +10,77 @@ class GameTest {
 
   Game game;
 
-  @Test
-  void addFirstRoll(){
+  @BeforeEach
+  void setup(){
     game = new Game();
-
-    game.addRoll(1);
-    Frame frame = game.frames()[0];
-
-    int[] expectedFrame = new int[]{1, 0};
-    int expectedScore = 1;
-    int expectedTotalScore = 1;
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertEquals(expectedTotalScore, game.totalScore());
-    assertFalse(game.over());
   }
 
   @Test
-  void addSecondRoll(){
-    game = new Game();
+  void testPinsAreCorrect(){
 
     game.addRoll(1);
-    game.addRoll(4);
-    Frame frame = game.frames()[0];
+    game.addRoll(9);
+    game.addRoll(2);
+    game.addRoll(2);
+    Frame firstFrame = game.frames()[0];
+    Frame secondFrame = game.frames()[1];
 
-    int[] expectedFrame = new int[]{1, 4};
-    int expectedScore = 5;
-    int expectedTotalScore = 5;
+    int[] expectedFrameFirstFrame = new int[]{1, 9};
+    int[] expectedFrameSecondFrame = new int[]{2, 2};
 
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedTotalScore, game.totalScore());
-    assertFalse(game.over());
+    assertArrayEquals(expectedFrameFirstFrame, firstFrame.getPinsRolled());
+    assertArrayEquals(expectedFrameSecondFrame, secondFrame.getPinsRolled());
   }
 
   @Test
-  void addThirdRoll(){
-    game = new Game();
+  void testSpareBonus(){
 
     game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    Frame frame = game.frames()[1];
-
-    int[] expectedFrame = new int[]{4, 0};
-    int expectedScore = 4;
-    int expectedTotalScore = 9;
-
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedTotalScore, game.totalScore());
-    assertEquals(expectedScore, frame.getScore());
-
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addFourthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-
-    Frame frame = game.frames()[1];
-
-    int[] expectedFrame = new int[]{4, 5};
-    int expectedScore = 9;
-    int expectedTotalScore = 14;
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addFithRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-
-
-    Frame frame = game.frames()[2];
-
-    int[] expectedFrame = new int[]{6, 0};
-    int expectedScore = 6;
-    int expectedTotalScore = 20;
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addSixthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-
-    Frame frame = game.frames()[2];
-
-    int[] expectedFrame = new int[]{6, 4};
-    int expectedScore = 10;
-    int expectedTotalScore = 24;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addSeventhRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-
-    Frame frame = game.frames()[3];
-
-    int[] expectedFrame = new int[]{5, 0};
-    int expectedScore = 5;
-    int expectedTotalScore = 34;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addEighthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-
-    Frame frame = game.frames()[3];
-
-    int[] expectedFrame = new int[]{5, 5};
-    int expectedScore = 10;
-    int expectedTotalScore = 39;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addNinthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-
-    Frame frame = game.frames()[4];
-
-    int[] expectedFrame = new int[]{10, 0};
-    int expectedScore = 10;
-    int expectedTotalScore = 59;
-    System.out.println(Arrays.toString(game.frames()));
-
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addTenthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-
-    Frame frame = game.frames()[5];
-
-    int[] expectedFrame = new int[]{0, 0};
-    int expectedScore = 0;
-    int expectedTotalScore = 59;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addEleventhRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-    game.addRoll(1);
-
-    Frame frame = game.frames()[5];
-
-    int[] expectedFrame = new int[]{0, 1};
-    int expectedScore = 1;
-    int expectedTotalScore = 61;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addTwelfthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-    game.addRoll(1);
-    game.addRoll(7);
-
-    Frame frame = game.frames()[6];
-
-    int[] expectedFrame = new int[]{7, 0};
-    int expectedScore = 7;
-    int expectedTotalScore = 68;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addThirteenthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-    game.addRoll(1);
-    game.addRoll(7);
-    game.addRoll(3);
-
-    Frame frame = game.frames()[6];
-
-    int[] expectedFrame = new int[]{7, 3};
-    int expectedScore = 10;
-    int expectedTotalScore = 71;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addFourteenthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-    game.addRoll(1);
-    game.addRoll(7);
-    game.addRoll(3);
-    game.addRoll(6);
-
-    Frame frame = game.frames()[7];
-
-    int[] expectedFrame = new int[]{6, 0};
-    int expectedScore = 6;
-    int expectedTotalScore = 83;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addFifteenthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-    game.addRoll(1);
-    game.addRoll(7);
-    game.addRoll(3);
-    game.addRoll(6);
-    game.addRoll(4);
-
-    Frame frame = game.frames()[7];
-
-    int[] expectedFrame = new int[]{6, 4};
-    int expectedScore = 10;
-    int expectedTotalScore = 87;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addSixteenthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-    game.addRoll(1);
-    game.addRoll(7);
-    game.addRoll(3);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(10);
-
-    Frame frame = game.frames()[8];
-
-    int[] expectedFrame = new int[]{10, 0};
-    int expectedScore = 10;
-    int expectedTotalScore = 107;
-
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
-  }
-
-  @Test
-  void addSeventeenthRoll(){
-    game = new Game();
-
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-    game.addRoll(1);
-    game.addRoll(7);
-    game.addRoll(3);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(10);
+    game.addRoll(9);
+    game.addRoll(2);
     game.addRoll(2);
 
+    Frame firstFrame = game.frames()[0];
 
-    Frame frame = game.frames()[9];
+    int expectedPointsFirstFrame = 12;
+    int actual = firstFrame.getScore();
 
-    int[] expectedFrame = new int[]{2, 0};
-    int expectedScore = 2;
-    int expectedTotalScore = 111;
+    assertEquals(expectedPointsFirstFrame, actual);
 
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
   }
 
   @Test
-  void addEighteenthRoll(){
-    game = new Game();
+  void testStrikeBonus(){
 
-    game.addRoll(1);
-    game.addRoll(4);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(6);
-    game.addRoll(4);
-    game.addRoll(5);
-    game.addRoll(5);
-    game.addRoll(10);
-    game.addRoll(0);
-    game.addRoll(1);
-    game.addRoll(7);
-    game.addRoll(3);
-    game.addRoll(6);
-    game.addRoll(4);
     game.addRoll(10);
     game.addRoll(2);
-    game.addRoll(8);
+    game.addRoll(2);
 
+    Frame firstFrame = game.frames()[0];
 
-    Frame frame = game.frames()[9];
+    int expectedPointsFirstFrame = 14;
+    int actual = firstFrame.getScore();
 
-    int[] expectedFrame = new int[]{2, 8, 0};
-    int expectedScore = 10;
-    int expectedTotalScore = 127;
+    assertEquals(expectedPointsFirstFrame, actual);
 
-    assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
-    assertFalse(game.over());
   }
 
   @Test
-  void addNinteenthRoll(){
+  void testStrikeFrame(){
+
+    game.addRoll(10);
+    game.addRoll(2);
+    game.addRoll(2);
+
+    Frame firstFrame = game.frames()[0];
+    int[] expectedFrame = new int[]{10, 0};
+
+    assertArrayEquals(expectedFrame, firstFrame.getPinsRolled());
+
+  }
+
+  @Test
+  void fullGame(){
     game = new Game();
 
     game.addRoll(1);
@@ -525,24 +103,11 @@ class GameTest {
     game.addRoll(8);
     game.addRoll(6);
 
-
-    Frame frame = game.frames()[9];
-
-    int[] expectedFrame = new int[]{2, 8, 6};
-    int expectedScore = 16;
     int expectedTotalScore = 133;
     System.out.println(Arrays.toString(game.frames()));
 
     assertEquals(expectedTotalScore, game.totalScore());
-
-    assertArrayEquals(expectedFrame, frame.getPinsRolled());
-    assertEquals(expectedScore, frame.getScore());
     assertTrue(game.over());
   }
-
-
-
-
-
 
 }
